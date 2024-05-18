bb5ae7bdf514c8931ca30a5c1a3502b626d2f365
Khochawongwat <khochawongwat.kongpana@gmail.com>
2024-05-18 17:04:19 +0700

    Added server.__init__.py

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import Button from '@mui/material/Button'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-    
+      <Button variant="text" color="primary">
+        Hello
+      </Button>
     </main>
   );
 }
